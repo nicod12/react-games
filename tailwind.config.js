e83cd41b169c9f727268d818224efe4ea6fe78ba
nicod12 <dumenicolas45@gmail.com>
2023-02-18 13:19:08 -0300

    udpv1

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,10 +18,20 @@ module.exports = {
       }
     },
     screens: {
-      'sm': {'min': '200px', 'max': '479px'},
-      'md': {'min': '480px', 'max': '767px'},
-      'lg': {'min': '768px', 'max': '1280px'},
-      'xl': {'min': '1281px'},
+        sm: { min: "100px", max: "767px" },
+        // => @media (min-width: 100px and max-width: 767px) { ... }
+
+        md: { min: "768px", max: "1023px" },
+        // => @media (min-width: 768px and max-width: 1023px) { ... }
+
+        lg: { min: "1024px", max: "1279px" },
+        // => @media (min-width: 1024px and max-width: 1279px) { ... }
+
+        xl: { min: "1280px", max: "1535px" },
+        // => @media (min-width: 1280px and max-width: 1535px) { ... }
+
+        "2xl": { min: "1536px" },
+        // => @media (min-width: 1536px) { ... }
     },
   },
   plugins: [
