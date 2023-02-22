6020bb16c94afd4c8ddf07300053c5d4b1cb16a6
nicod12 <dumenicolas45@gmail.com>
2023-02-22 10:10:00 -0300

    version final

--- src/index.jsx
+++ src/index.jsx
@@ -16,19 +16,6 @@ const Index = () => {
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
-root.render(<Index />)
+root.render(<Index />);
 
-serviceWorkerRegistration.register({
-    onUpdate: async (registration) => {
-     // We execute this code if there is a new version of our application
-    // Details in: https://developers.google.com/web/fundamentals/primers/service-workers/lifecycle
-      if (registration && registration.waiting) {
-        await registration.unregister();
-        registration.waiting.postMessage({ type: "SKIP_WAITING" });
-       
-
-    // Unregister the SW to refresh the page and get the new version. Which allows the browser to download the new one and invalidate the cache it had previously.
-        window.location.reload();
-      }
-    },
-  });
+serviceWorkerRegistration.register();
