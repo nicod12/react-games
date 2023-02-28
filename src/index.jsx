6315eb044113a0be95b74bd6823379997192a9ef
nicod12 <dumenicolas45@gmail.com>
2023-02-28 11:51:29 -0300

    upd

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,7 @@ import  ReactDOM  from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./routes/App";
 import './index.css'
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 
 const Index = () => {
@@ -15,4 +16,9 @@ const Index = () => {
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
 root.render(<Index />);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
 
